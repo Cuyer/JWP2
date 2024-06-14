ffb9dd77f46ac2f1edb7ad2c7e143e4aa0a2f920
Krystian Checinski <krystixn.checinski@gmail.com>
2024-06-14 20:40:28 +0200

    Dodano zaległe zadania

---
+++ zaj2/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h1>Witaj w mojej aplikacji Flask!</h1>
+    <a href="/about"><button>O mnie</button></a>
+    <a href="/contact"><button>Kontakt</button></a>
+    '''
+
+@app.route('/about')
+def about():
+    return "Zaprogramowano przez Krystian."
+
+@app.route('/contact')
+def contact():
+    return "Email: krystian@example.com."
+
+if __name__ == '__main__':
+    app.run(debug=True)
